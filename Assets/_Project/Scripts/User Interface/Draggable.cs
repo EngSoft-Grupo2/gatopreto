b772cf93872e393d5577a47dd21abe61d558fac5
Bruno Cesar Pimenta Fernandes <brunocpf@dcc.ufmg.br>
2017-10-22 14:50:27 -0200

    Adiciona eventos de drag e animação ao soltar a carta

--- Assets/_Project/Scripts/User Interface/Draggable.cs
+++ Assets/_Project/Scripts/User Interface/Draggable.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+namespace ProjetoGatoPreto
 {
-	private Vector3 startPosition;
-	private Vector3 dragPosition;
+    public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
 
-	/// <summary>
-	/// Start is called on the frame when a script is enabled just before
-	/// any of the Update methods is called the first time.
-	/// </summary>
-	void Start()
-	{
-		startPosition = transform.position;
-	}
+        public AnimationCurve resetAnimation;
+        private Vector3 resetPosition;
+        private Vector3 dragPosition;
+        private EasyTween tween;
+				private bool dragged = false;
 
-    public void OnBeginDrag (PointerEventData eventData)
-    {
-		dragPosition = this.transform.position - (Vector3) eventData.position;
-    }
-    public void OnDrag (PointerEventData eventData)
-    {
-		this.transform.position =  dragPosition + (Vector3) eventData.position;
-    }
-    public void OnEndDrag (PointerEventData eventData)
-    {
-		this.transform.position = startPosition;
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake ()
+        {
+            this.tween = this.GetComponent<EasyTween> ();
+        }
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start ()
+        {
+            this.resetPosition = this.tween.rectTransform.anchoredPosition;
+        }
+        public void OnBeginDrag (PointerEventData eventData)
+        {
+            dragPosition = this.transform.position - (Vector3) eventData.position;
+        }
+        public void OnDrag (PointerEventData eventData)
+        {
+						this.dragged = false;
+            if (!this.tween.IsObjectOpened ())
+						{
+                this.transform.position = dragPosition + (Vector3) eventData.position;
+								this.dragged = true;
+						}
+				}
+        public void OnEndDrag (PointerEventData eventData)
+        {
+						if (!this.dragged)
+								return;
+            if (!this.tween.IsObjectOpened ())
+            {
+                this.tween.SetAnimationPosition (this.tween.rectTransform.anchoredPosition, this.resetPosition, resetAnimation, resetAnimation);
+            }
+            else
+            {
+                this.tween.SetAnimationPosition (this.resetPosition, this.tween.rectTransform.anchoredPosition, resetAnimation, resetAnimation);
+            }
+            tween.OpenCloseObjectAnimation ();
+        }
     }
 }
---
+++ Assets/_Project/Scripts/User Interface/DropZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjetoGatoPreto
+{
+    public class DropZone : MonoBehaviour
+    {
+
+        void OnTriggerEnter2D (Collider2D other)
+        {
+            Debug.Log ("Triggered");
+        }
+
+    }
+}
