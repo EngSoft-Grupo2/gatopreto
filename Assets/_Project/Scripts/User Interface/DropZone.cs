2d9a03a355b0d6eaaa72aef0022535559d39bbae
Bruno Cesar Pimenta Fernandes <brunocpf@dcc.ufmg.br>
2017-10-22 17:39:06 -0200

    Colisor do DropZone funcionando

--- Assets/_Project/Scripts/User Interface/DropZone.cs
+++ Assets/_Project/Scripts/User Interface/DropZone.cs
@@ -6,11 +6,28 @@ namespace ProjetoGatoPreto
 {
     public class DropZone : MonoBehaviour
     {
+        public CanvasGroup decisionDisplay;
+        public float fadeTime = 0.25f;
 
         void OnTriggerEnter2D (Collider2D other)
         {
-            Debug.Log ("Triggered");
+            StartCoroutine(this.AnimateCanvasGroupAlpha(decisionDisplay, 0.0f, 1.0f, fadeTime));
         }
 
+        void OnTriggerExit2D(Collider2D other)
+        {
+            StartCoroutine(this.AnimateCanvasGroupAlpha(decisionDisplay, 1.0f, 0.0f, fadeTime));
+        }
+
+        IEnumerator AnimateCanvasGroupAlpha (CanvasGroup canvasGroup, float initValue, float finalValue, float overTime)
+        {
+            float startTime = Time.time;
+            while (Time.time < startTime + overTime)
+            {
+                canvasGroup.alpha = Mathf.Lerp (initValue, finalValue, (Time.time - startTime) / overTime);
+                yield return null;
+            }
+            canvasGroup.alpha = finalValue;
+        }
     }
 }
